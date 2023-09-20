3aee47fdbf4668cd2e4b6993750c91985b78cdea
Luansr01 <luan.sr2010@gmail.com>
2023-09-20 00:12:38 -0300

    added Mat2, Vec3 and Mat3

--- tests.cpp
+++ tests.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
 #include <mymath.h>
 
-int main(){
-    Vec2 x(1.0, 2.0);
-    Angle a(1);
+void printVec2(Vec2 vec){
+    std::cout << "[" << vec.x << ", " << vec.y << "]" << "\n";
+}
 
-    std::cout << x[0] << "\n";
+void printMat2(Mat2 mat){
+    printVec2(mat.x);
+    printVec2(mat.y);
+}
+
+int main(){
+    Vec2 vec(1.0, 2.0);
+    printVec2(vec);
+
+    std::cout << "\n";
+
+    Mat2 mat(1, 1, 0, 1);
+    mat *= Mat2(2, 5, 6, 7);
+    printMat2(mat);
 }
